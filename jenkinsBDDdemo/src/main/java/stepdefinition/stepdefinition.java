d70222b6b6063b9439039e68ca15b8aa977c735d
IBM <IBM@DEVOPS_19>
2018-07-24 15:11:09 +0530

    commited

--- jenkinsBDDdemo/src/main/java/stepdefinition/stepdefinition.java
+++ jenkinsBDDdemo/src/main/java/stepdefinition/stepdefinition.java
@@ -25,6 +25,7 @@ public class stepdefinition {
 		public void validate_login_application() throws Throwable {
 			title=driver.getTitle();
 			System.out.println(title);
+			System.out.println("hello");
 		    // Write code here that turns the phrase above into concrete actions
 		   
 		}
