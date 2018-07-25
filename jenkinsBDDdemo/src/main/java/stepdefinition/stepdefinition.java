73e0def8a1331f63f03a229e146acbcc85f26846
IBM <IBM@DEVOPS_19>
2018-07-25 12:38:03 +0530

    done

--- jenkinsBDDdemo/src/main/java/stepdefinition/stepdefinition.java
+++ jenkinsBDDdemo/src/main/java/stepdefinition/stepdefinition.java
@@ -2,11 +2,12 @@ package stepdefinition;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
+
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import junit.framework.Assert;
 
 public class stepdefinition {
 
